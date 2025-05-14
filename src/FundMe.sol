ded22079ff3184661802fe6a0531bca3b0152a44
Odin Thif <mjd.thif@icloud.com>
2025-05-14 22:59:42 +0200

    refactoring

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,36 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
-// Note: The AggregatorV3Interface might be at a different location than what was in the video!
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 error NotOwner();
 
 contract FundMe {
+
     using PriceConverter for uint256;
 
     mapping(address => uint256) public addressToAmountFunded;
+
     address[] public funders;
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
     address public /* immutable */ i_owner;
+
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
 
-    constructor() {
+    AggregatorV3Interface private s_priceFeed;
+
+    constructor(address priceFeed) {
         i_owner = msg.sender;
-    }
-
-    function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
+        s_priceFeed = AggregatorV3Interface(priceFeed);
     }
 
     modifier onlyOwner() {
@@ -39,6 +30,18 @@ contract FundMe {
         _;
     }
 
+    function fund() public payable {
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
+    }
+
+ 
     function withdraw() public onlyOwner {
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -2,15 +2,23 @@
 pragma solidity ^0.8.13;
 import {Test, console} from "../lib/forge-std/src/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
-
-
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
-    uint256 number = 1;
+    FundMe fundMe;
+
     function setUp() external {
-        FundMe fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
-    function testDemo() public {
-        assertEq(number, 2);
+
+    function testPriceFeedVersionIsAccurate() public view {
+        if (block.chainid == 11155111) {
+            uint256 version = fundMe.getVersion();
+            assertEq(version, 4);
+        } else if (block.chainid == 1) {
+            uint256 version = fundMe.getVersion();
+            assertEq(version, 6);
+        }
     }
 }
